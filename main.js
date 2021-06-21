c6fef5f51001e32eae8c017062ac7c9ec646b1c4
n-czekalska <35266616+n-czekalska@users.noreply.github.com>
2021-06-21 16:38:36 +0100

    initial

---
+++ main.js
@@ -0,0 +1,38 @@
+const apiKey = "76f78e8f22cb4efa84f125231212006";
+const basicURI = "https://api.weatherapi.com/v1/";
+const current = "current.json";
+
+
+window.addEventListener('load', async function() {
+    const result = await getCurrentWeather();
+    console.log(result);
+    displayResult(result);
+})
+
+
+async function getCurrentWeather(location = "Edinburgh") {
+        const response = await fetch(basicURI + current + "?key="+ apiKey + "&q=" + location)
+        .catch(err => console.error(err));
+        return await response.json();
+}
+
+function displayResult(result) {
+    let currentView = document.querySelector(".current > .card");
+    currentView.children[0].innerHTML = result.location.name +", " + result.location.country;
+    currentView.children[1].innerHTML = "As of " + result.current.last_updated;
+    currentView.children[2].children[0].children[0].innerHTML = result.current.temp_c +'&deg;C';
+    currentView.children[2].children[0].children[1].innerHTML = result.current.condition.text;
+    currentView.children[2].children[1].src = findIcon(result.current.condition.icon);
+
+}
+
+function findIcon(code) {
+    const weatherjson = {};
+     fetch("./assets/weather_conditions.json")
+    .then(response => {
+       weatherjson =  response.json();
+    });
+
+    const icon = weatherjson.find(x => x["code"] === code);
+    return icon["icon"];
+}
