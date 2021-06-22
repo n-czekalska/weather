87a05d55182a7cb2eca10285b633f0568cd75fc6
n-czekalska <nat.czekalska@gmail.com>
2021-06-22 10:35:08 +0100

    round temperature

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ async function displayResult(result) {
     let currentView = document.querySelector(".current > .card");
     currentView.children[0].innerHTML = result.location.name +", " + result.location.country;
     currentView.children[1].innerHTML = "As of " + result.current.last_updated;
-    currentView.children[2].children[0].children[0].innerHTML = result.current.temp_c +'&deg;C';
+    currentView.children[2].children[0].children[0].innerHTML = Math.round(result.current.temp_c) +'&deg;C';
     currentView.children[2].children[0].children[1].innerHTML = result.current.condition.text;
     Promise.resolve(findIcon(result.current.condition.code).then(data => currentView.children[2].children[1].src = "assets/"+ data.icon));
 
