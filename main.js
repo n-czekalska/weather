2a51f04d746a00e1ac1689fb7af613d52436144b
n-czekalska <nat.czekalska@gmail.com>
2021-06-22 11:37:58 +0100

    module test

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+import moment from moment.js;
 const apiKey = "76f78e8f22cb4efa84f125231212006";
 const basicURI = "https://api.weatherapi.com/v1/";
 const current = "current.json";
@@ -30,8 +31,8 @@ async function getCurrentWeather(location) {
 async function displayResult(result) {
     let currentView = document.querySelector(".current > .card");
     currentView.children[0].innerHTML = result.location.name +", " + result.location.country;
-    currentView.children[1].innerHTML = "As of " + result.current.last_updated;
-    currentView.children[2].children[0].children[0].innerHTML = Math.round(result.current.temp_c) +'&deg;C';
+    currentView.children[1].innerHTML = "As of " + moment(result.current.last_updated).format('YYYY-MM-DD HH:m:s');
+    currentView.children[2].children[0].children[0].innerHTML = result.current.temp_c +'&deg;C';
     currentView.children[2].children[0].children[1].innerHTML = result.current.condition.text;
     Promise.resolve(findIcon(result.current.condition.code).then(data => currentView.children[2].children[1].src = "assets/"+ data.icon));
 
