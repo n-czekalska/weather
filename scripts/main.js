8dd90a1a5d6ae9226fd9ad62c598951bc90ae7d1
n-czekalska <nat.czekalska@gmail.com>
2021-06-23 17:41:33 +0100

    new details card

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,10 @@
 const apiKey = "76f78e8f22cb4efa84f125231212006";
 const basicURI = "https://api.weatherapi.com/v1/";
-const current = "current.json";
-const loc = document.getElementById("location");
-const date = document.getElementById("dateTime");
-const temp = document.getElementById("temp");
-const condition = document.getElementById("condition");
-const icon = document.getElementById("weathericon");
-const today = document.getElementById("today");
-const fiveDay = document.getElementById("fiveday");
-const tenDay = document.getElementById("tenday");
+const forecast = "forecast.json";
+let loc, date, temp, condition, icon, feelsLike, min, max, wind, humidity, uv, pressure, today, fiveDay, tenDay;
 
 window.addEventListener('load', async function() {
+    getDocumentElements()
     const result = await getCurrentWeather();
     console.log(result);
     displayResult(result);
@@ -41,7 +35,7 @@ async function searchCurrentWeather() {
 }
 
 async function getCurrentWeather(searchLocation) {
-        const response = await fetch(basicURI + current + "?key="+ apiKey + "&q=" + (searchLocation ? searchLocation : "Edinburgh"))
+        const response = await fetch(basicURI + forecast + "?key="+ apiKey + "&q=" + (searchLocation ? searchLocation : "Edinburgh")+"&days=1&aqi=no&alerts=no")
         .catch(error => {
             console.error(error);
             const searchInput = document.getElementById("search-input");
@@ -51,11 +45,26 @@ async function getCurrentWeather(searchLocation) {
 }
 
 async function displayResult(result) {
-    loc.innerHTML = result.location.name +", " + result.location.country;
-    date.innerHTML = "Last updated " + moment(result.current.last_updated).format("HH:mm");
-    temp.innerHTML = result.current.temp_c +'&deg;C';
-    condition.innerHTML = result.current.condition.text;
-    Promise.resolve(findIcon(result.current.condition.code).then(data => icon.src = "assets/"+ data.icon));
+    const current = result.current;
+    const forecastInfo = result.forecast.forecastday[0];
+
+    for (i = 0; i < loc.length; i++) {
+        loc[i].innerHTML = result.location.name +", " + result.location.country;
+      }
+    
+    date.innerHTML = "Last updated " + moment(current.last_updated).format("HH:mm");
+    temp.innerHTML = current.temp_c +'&deg;C';
+    condition.innerHTML = current.condition.text;
+    Promise.resolve(findIcon(current.condition.code).then(data => icon.src = "assets/"+ data.icon));
+
+    feelsLike.innerHTML = current.feelslike_c +'&deg;C';
+    
+    min.innerHTML = forecastInfo.day.mintemp_c +'&deg;C';
+    max.innerHTML =  forecastInfo.day.maxtemp_c+'&deg;C';
+    wind.innerHTML = current.wind_kph + " kph /" + current.wind_mph + " mph";
+    pressure.innerHTML = current.pressure_mb + " mb" ;
+    uv.innerHTML = current.uv + " of 10";
+    humidity.innerHTML = current.humidity + " %";
 
 }
 
@@ -66,4 +75,24 @@ async function findIcon(code) {
         console.log(json);
         return json.find(x => x["code"] === code)
      });
+}
+
+function getDocumentElements() {
+ loc = document.getElementsByClassName("location");
+ date = document.getElementById("dateTime");
+ temp = document.getElementById("temp");
+ condition = document.getElementById("condition");
+ icon = document.getElementById("weathericon");
+ feelsLike = document.getElementById("feelsLike");
+ min= document.getElementById("min");
+ max = document.getElementById("max");
+
+ wind =  document.getElementById("wind"); 
+ humidity =  document.getElementById("humidity");
+ uv =  document.getElementById("uv");
+ pressure =  document.getElementById("pressure");
+
+ today = document.getElementById("today");
+ fiveDay = document.getElementById("fiveday");
+ tenDay = document.getElementById("tenday");
 }
